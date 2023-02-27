79ab75cbf581c5e8bbe9b5a9242df1c94bcdd1fe
Paul Swartz <paul@paulswartz.net>
2023-02-27 13:27:44 -0500

    chore: use `exit-code` branch of `lcov_ex`

--- mix.exs
+++ mix.exs
@@ -60,7 +60,9 @@ defmodule ApiUmbrella.Mixfile do
     # Static analysis and style checking
     [
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
-      {:lcov_ex, "~> 0.2", only: [:dev, :test], runtime: false},
+      # {:lcov_ex, "~> 0.2", only: [:dev, :test], runtime: false},
+      {:lcov_ex,
+       github: "paulswartz/lcov_ex", ref: "exit-code", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.0", only: [:dev], runtime: false},
       # Generate docs with `mix docs`
       {:ex_doc, "~> 0.20", only: [:dev, :test], runtime: false}
